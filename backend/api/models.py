0d51b861f065a7dbfe9627b81d26dba5c6a3dd23
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-06-26 07:53:15 -0300

    fix: title charfield was incorrect

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Task(models.Model):
 
-    title = models.Charfield(max_length=200, unique=True)
+    title = models.CharField(max_length=200, unique=True)
     datetime = models.DateTimeField()
