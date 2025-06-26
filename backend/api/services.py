98905960c73e8ebc7dca8c9fcbb27b696391ac2e
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-06-26 08:00:24 -0300

    feat: Added tasks list logical

--- backend/api/services.py
+++ backend/api/services.py
@@ -1,1 +1,5 @@
-...
+from api.models import Task
+
+def get_task_list():
+    
+    return Task.objects.all().order_by('datetime')
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from api.services import get_task_list
 
 def task_list(request):
 
-    ...
+    tasks = get_task_list()
+
+    return JsonResponse({
+        tasks: list(tasks.values('id', 'title', 'datetime'))
+    }, safe=False)
+
+
+    
