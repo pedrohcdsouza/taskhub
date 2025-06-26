c7e8e3cfe713faf2fe74d041800d32ddb02557e6
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-06-26 07:45:09 -0300

    feat: Added models Task to admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from api.models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'datetime')
+    search_fields = ('title',)
+    ordering = ('datetime',)
