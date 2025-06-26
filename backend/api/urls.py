5b36d1688cbdeea2e293bccd56b830046f74634b
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-06-26 08:11:48 -0300

    feat: Added CRUD urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from api.views import task_list as api_task_list
+from api.views import task_list, task_create, task_edit, task_delete
+
 
 urlpatterns = [
-    path('tasklist/', api_task_list, name='task_list'),
+    path('tasklist/', task_list, name='task_list'),
+    path('taskcreate/', task_create, name='task_create'),
+    path('taskedit/', task_edit, name='task_edit'),
+    path('taskdelete/', task_delete, name='task_delete'),
 ]
