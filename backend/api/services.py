7e327e4b2fbe3097d7415c1e83afa4304077a88a
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-06-26 08:03:16 -0300

    feat: Added tasks create logical

--- backend/api/services.py
+++ backend/api/services.py
@@ -1,5 +1,12 @@
 from api.models import Task
 
 def get_task_list():
-    
+
     return Task.objects.all().order_by('datetime')
+
+def create_task(title, datetime):
+
+    task = Task(title=title, datetime=datetime)
+    task.save()
+    
+    return task
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from api.services import get_task_list
+from api.services import get_task_list, create_task
 
 def task_list(request):
 
@@ -10,5 +10,22 @@ def task_list(request):
         tasks: list(tasks.values('id', 'title', 'datetime'))
     }, safe=False)
 
+def task_create(request):
+
+    if request.method == 'POST':
+
+        title = request.POST.get('title')
+        datetime = request.POST.get('datetime')
+
+        if title and datetime:
+            
+            task = create_task(title, datetime)
+            return JsonResponse({
+                'id': task.id,
+                'title': task.title,
+                'datetime': task.datetime.isoformat()
+            }, status=201)
+
+        return JsonResponse({'error': 'Invalid data'}, status=400)
 
     
