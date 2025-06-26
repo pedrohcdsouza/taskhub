f8591e00c3f0d19b7453e09a90464fbf5d4f5c95
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-06-26 07:47:39 -0300

    fix: func task_list was invalid

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from api.views import task_list as api_task_list
 
 urlpatterns = [
-    path('tasklist/', 'api.views.task_list', name='task_list'),
+    path('tasklist/', api_task_list, name='task_list'),
 ]
